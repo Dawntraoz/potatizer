30349258f31441695092bfd66963f9b1a226d707
Dawntraoz <albasilventefu@gmail.com>
2020-12-13 23:30:29 +0100

    First commit Potatizer background sizes svg and favicons

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <AnimatedPotatoes
+      class="fixed inset-0 -z-1 transform scale-350 md:scale-175 lg:scale-100"
+      width="100vw"
+      height="100vh"
+    />
+    <Nuxt />
+  </div>
+</template>
---
+++ nuxt.config.js
@@ -0,0 +1,84 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'potatizer',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'msapplication-TileColor', content: '#a3e635' },
+      { name: 'theme-color', content: '#a3e635' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'icon',
+        sizes: '32x32',
+        type: 'image/png',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        sizes: '16x16',
+        type: 'image/png',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/icon.png' },
+      { rel: 'mask-icon', color: '#a3e635', href: '/safari-pinned-tab.svg' },
+    ],
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  pwa: {
+    manifest: {
+      name: 'Potatizer',
+      short_name: 'Potatizer',
+      lang: 'en',
+      icons: [
+        {
+          src: '/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+      theme_color: '#a3e635',
+      background_color: '#a3e635',
+      display: 'standalone',
+    },
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+}
