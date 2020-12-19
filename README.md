6884c274acf1b92d2e9cd9de0d2fd8011439c222
Dawntraoz <albasilventefu@gmail.com>
2020-12-19 22:22:10 +0100

    feat: digitalocean workflow added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # potatizer
 
 Generate your potato mood randomly with this without sense app 🥔
+
+[![Deploy to DigitalOcean](https://mp-assets1.sfo2.digitaloceanspaces.com/deploy-to-do/do-btn-blue.svg)](https://cloud.digitalocean.com/apps/new?repo=https://github.com/Dawntraoz/potatizer/tree/main)
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,5 +6,16 @@
       height="100vh"
     />
     <Nuxt />
+    <footer>
+      <a
+        target="_blank"
+        href="https://cloud.digitalocean.com/apps/new?repo=https://github.com/Dawntraoz/potatizer/tree/main"
+      >
+        <img
+          src="https://mp-assets1.sfo2.digitaloceanspaces.com/deploy-to-do/do-btn-blue.svg"
+          alt="Deploy to DigitalOcean"
+        />
+      </a>
+    </footer>
   </div>
 </template>
