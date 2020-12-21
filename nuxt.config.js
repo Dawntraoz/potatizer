7be88455b06f92e39ac155f4a4f99277df818de0
Dawntraoz <albasilventefu@gmail.com>
2020-12-21 16:20:52 +0100

    fix: plugin added

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,7 +79,7 @@ export default {
   css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [{ src: '~plugins/ga.js', mode: 'client' }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
